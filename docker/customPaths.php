384ef307280135fc49f24ff1c34e3d1511871c8e
Daniel Pegg <dan@parall.ax>
2019-09-19 13:04:59 +0100

    Remove custom file for commandline-dusk

--- docker/customPaths.php
+++
@@ -1,9 +0,0 @@
-return [
-    /*
-    |--------------------------------------------------------------------------
-    | Laravel Console Dusk Paths
-    |--------------------------------------------------------------------------
-    |
-    | Here you may configure the name of screenshots and logs directory as you wish.
-    */
-];
