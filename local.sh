7f41c8ab7ba44173ea1cba28d1b22da0c8035a3b
Daniel Pegg <dan@parall.ax>
2019-09-20 15:00:36 +0100

    Make changes to allow running outside of the container

--- local.sh
+++ local.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 composer update
 cp docker/customChrome.php vendor/nunomaduro/laravel-console-dusk/src/Drivers/Chrome.php
+cp .env.example .env
+
+echo +--------------------+
+echo 
+echo \| FILL IN .env FILE! \|
+echo
+echo +--------------------+
