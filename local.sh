0ac3c716c267a7cdacbec54f73506d2b65a1ce14
Daniel Pegg <dan@parall.ax>
2019-09-20 15:05:58 +0100

    Run chromedriver update in local script

--- local.sh
+++ local.sh
@@ -2,6 +2,7 @@
 composer update
 cp docker/customChrome.php vendor/nunomaduro/laravel-console-dusk/src/Drivers/Chrome.php
 cp .env.example .env
+php php /src/zero-dusk dusk:update --detect=/usr/bin/google-chrome
 
 echo +--------------------+
 echo 
